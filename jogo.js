de219acfc8038c110b7b236a1edce804601c200d
Andre Viana <code.andre3@gmail.com>
2021-05-26 18:43:00 -0300

    lógica adicionando a img randomica

--- jogo.js
+++ jogo.js
@@ -1,14 +1,34 @@
 // Saber o tamanho da Tela que o jogo será exibido
-var height = 0
-var width = 0
+var altura = 0
+var largura = 0
 
-function adjustSizeWindow(){
+function ajustaTamanhoPalcoJogo(){
 
-var height = window.innerHeight
-var width = window.innerWidth
+altura = window.innerHeight
+largura = window.innerWidth
 
-console.log(height, width)
+console.log(largura, altura)
 }
 
-adjustSizeWindow()
+ajustaTamanhoPalcoJogo()
 
+// Gerando posições aleatórias para o mosquito
+
+function posicaoRandomica() {
+
+var posicaoX = Math.floor(Math.random() * largura)
+var posicaoY = Math.floor(Math.random() * altura)
+
+console.log(posicaoX, posicaoY)
+
+// Criar o Elemento HTML
+
+var mosquito = document.createElement('img')
+mosquito.src = 'imagens/mosca.png'
+mosquito.className = 'mosquito1'
+mosquito.style.left = posicaoX + 'px'
+mosquito.style.top = posicaoY + 'px'
+mosquito.style.position = 'absolute'
+
+document.body.appendChild(mosquito)
+}
