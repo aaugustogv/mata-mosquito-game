c952a9edda3164988c5c47a75e067b81749af7eb
Andre Viana <code.andre3@gmail.com>
2021-05-26 18:09:52 -0300

    logica da Tela

---
+++ jogo.js
@@ -0,0 +1,14 @@
+// Saber o tamanho da Tela que o jogo será exibido
+var height = 0
+var width = 0
+
+function adjustSizeWindow(){
+
+var height = window.innerHeight
+var width = window.innerWidth
+
+console.log(height, width)
+}
+
+adjustSizeWindow()
+
